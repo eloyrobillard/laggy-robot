f855692119fb30c39bf7b2bb0c73380e4559c71b
Eloy Robillard <eloy.robillard@gmail.com>
2026-01-27 14:43:56 +0100

    Add basic test level

---
+++ scripts/you_win.gd
@@ -0,0 +1,21 @@
+extends CanvasLayer
+
+@onready var win_audio: AudioStreamPlayer = $WinAudio
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	visible = false
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+
+func _on_area_2d_body_entered(body: Node2D) -> void:
+	if body is CharacterBody2D:
+		visible = true
+		win_audio.play()
+		
+		await get_tree().create_timer(3.0).timeout
+		get_tree().quit()
