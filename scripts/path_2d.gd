1336758813649657a2a369056e11f36d3a7324be
Eloy Robillard <eloy.robillard@gmail.com>
2026-01-27 15:53:40 +0100

    Add vengeful ghost following player

---
+++ scripts/path_2d.gd
@@ -0,0 +1,13 @@
+extends Path2D
+
+var t = 0.0
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+func _process(delta):
+	t += delta
+	global_position = curve.sample_baked(t / 5 * curve.get_baked_length(), false)
