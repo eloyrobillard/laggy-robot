b17214b94c7cbff039883e8776b2ec95051cd06d
Eloy Robillard <eloy.robillard@gmail.com>
2026-01-31 15:30:58 +0100

    Add clutches

---
+++ scripts/path_follow_2d.gd
@@ -0,0 +1,30 @@
+extends PathFollow2D
+
+const BASE_INV_SPEED = 5
+
+var inverse_speed = BASE_INV_SPEED
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass
+
+
+func _process(delta):
+	progress_ratio += delta / 1.2 / inverse_speed
+
+
+func _on_player_entered_ultra_instinct_mode(slow_down_factor: float) -> void:
+	inverse_speed = BASE_INV_SPEED * slow_down_factor
+
+
+func _on_player_left_ultra_instrinct_mode() -> void:
+	inverse_speed = BASE_INV_SPEED
+
+
+func _on_player_won() -> void:
+	set_process(false)
+
+
+func _on_player_died() -> void:
+	set_process(false)
