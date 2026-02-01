d12c3fcb68679390b2a9407ca3981218a8b3e12d
Eloy Robillard <eloy.robillard@gmail.com>
2026-02-01 18:26:43 +0100

    Show debug UI except in release

--- scripts/debug_layer.gd
+++ scripts/debug_layer.gd
@@ -12,7 +12,9 @@ var velocity: Vector2
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	if OS.has_feature("release"):
+		visible = false
+		process_mode = Node.PROCESS_MODE_DISABLED
 
 
 func _physics_process(delta: float) -> void:
