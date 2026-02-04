f6ded847c7eb15faa0d966cf9c998d79673535f6
Eloy Robillard <eloy.robillard@gmail.com>
2026-02-04 20:08:04 +0100

    Notify player of ultra instinct with red screen outline

--- scripts/ultra_instinct_layer.gd
+++ scripts/ultra_instinct_layer.gd
@@ -2,8 +2,7 @@ extends CanvasLayer
 
 signal ultra_instinct_depleted
 
-@onready var ultra_instinct_tint: TextureRect = $UltraInstinctTint
-@onready var ultra_instinct_tint_2: TextureRect = $UltraInstinctTint2
+@onready var ultra_instinct_tint: Panel = $UltraInstinctTint
 @onready var progress_bar: ProgressBar = $ProgressBar
 @onready var player_position: ColorRect = $PlayerPosition
 
@@ -17,18 +16,13 @@ var current_ultra_instinct_percent
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	ultra_instinct_tint.modulate.a = 0
-	ultra_instinct_tint_2.modulate.a = 0
-	ultra_instinct_tint.visible = true
-	ultra_instinct_tint_2.visible = true
+	ultra_instinct_tint.visible = false
 	current_ultra_instinct_sec = MAX_ULTRA_INSTINCT_SEC
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	if in_ultra_instinct_mode:
-		ultra_instinct_tint.modulate.a += delta / ultra_instinct_factor
-		ultra_instinct_tint_2.modulate.a += delta / ultra_instinct_factor
 		current_ultra_instinct_sec -= delta
 		current_ultra_instinct_percent = current_ultra_instinct_sec / MAX_ULTRA_INSTINCT_SEC * 100.0
 		progress_bar.set_value_no_signal(current_ultra_instinct_percent)
@@ -41,13 +35,11 @@ func _process(delta: float) -> void:
 
 func _on_player_entered_ultra_instinct_mode(slow_down_factor: float) -> void:
 	in_ultra_instinct_mode = true
-	ultra_instinct_factor = slow_down_factor
+	ultra_instinct_tint.visible = true
 	player_position.visible = true
 
 
 func _on_player_left_ultra_instrinct_mode() -> void:
 	in_ultra_instinct_mode = false
+	ultra_instinct_tint.visible = false
 	player_position.visible = false
-
-	ultra_instinct_tint.modulate.a = 0
-	ultra_instinct_tint_2.modulate.a = 0
