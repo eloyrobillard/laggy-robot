9f1d619839569c452c4017aee74396ea9a69d2b1
Eloy Robillard <eloy.robillard@gmail.com>
2026-02-04 17:27:00 +0100

    Give better names to InputFrame initialisers

--- scripts/InputFrame.gd
+++ scripts/InputFrame.gd
@@ -5,13 +5,14 @@ var frame: int
 var action: InputActions.Action
 var pressed: bool
 
-func _init(f: int, a: InputActions.Action, p: bool):
-	frame = f
-	action = a
-	pressed = p
+
+func _init(_frame: int, _action: InputActions.Action, _pressed: bool):
+	frame = _frame
+	action = _action
+	pressed = _pressed
+
 
 func setFrame(f: int, a: InputActions.Action, p: bool):
 	frame = f
 	action = a
 	pressed = p
-	
