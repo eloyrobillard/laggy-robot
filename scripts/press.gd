aeaa38e3f5567e32e103e27d9184a16e2dbf7fd6
Eloy Robillard <eloy.robillard@gmail.com>
2026-02-07 16:01:11 +0100

    Return press to initial position when leaving ultra instinct

--- scripts/press.gd
+++ scripts/press.gd
@@ -3,34 +3,29 @@ extends Node2D
 signal hit_player
 
 @onready var press_body: AnimatableBody2D = $PressBody
+@onready var animation_player: AnimationPlayer = $PressBody/AnimationPlayer
 
-@export var time_to_bottom: float = 1
-@export var time_to_top: float = 1
-@export var pause_bottom: float = 1
-@export var pause_top: float = 1
-@export var transition_down: Tween.TransitionType
-@export var transition_up: Tween.TransitionType
-@export var ease_down: Tween.EaseType
-@export var ease_up: Tween.EaseType
-
-var y_tween: Tween
-var size_shaft_px = 560
+var animation_position_when_entered_ultra_instinct: float
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	y_tween = create_tween().set_loops()
-	y_tween.tween_property(press_body, "position:y", press_body.position.y + size_shaft_px, time_to_bottom).set_trans(transition_down).set_ease(ease_down)
-	y_tween.tween_interval(pause_bottom)
-	y_tween.tween_property(press_body, "position:y", press_body.position.y, time_to_top).set_trans(transition_up).set_ease(ease_up)
-	y_tween.tween_interval(pause_top)
+	pass
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
+func _process(_delta: float) -> void:
 	pass
 
 
 func _on_press_body_body_entered(body: Node) -> void:
 	if body is CharacterBody2D:
 		hit_player.emit()
+
+
+func _on_player_entered_ultra_instinct_mode(_slow_down_factor: float) -> void:
+	animation_position_when_entered_ultra_instinct = animation_player.current_animation_position
+
+
+func _on_player_left_ultra_instinct_mode() -> void:
+	animation_player.seek(animation_position_when_entered_ultra_instinct)
